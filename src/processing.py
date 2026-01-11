c9feb7be4f99e0bb397d8c82d5536e0b1f550732
Sergey Belyakov <belserg2002@gmail.com>
2026-01-11 18:04:56 +0300

    добавлен описания типов к новым функциям

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,18 @@
-def filter_by_state(operations, state='EXECUTED'):
-    """ Фильтрует список словарей по значению ключа 'state'"""
-    filtered_operations = []
+from typing import List, Dict, Any
+
+
+def filter_by_state(operations: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+    """ Фильтрует список словарей по значению ключа 'state' """
+    filtered_operations: List[Dict[str, Any]] = []
     for operation in operations:
         if operation.get('state') == state:
             filtered_operations.append(operation)
     return filtered_operations
 
 
-def sort_by_date(operations, reverse=True):
+def sort_by_date(operations: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """ Сортирует список словарей по дате """
-    sorted_operations = operations.copy()
+    sorted_operations: List[Dict[str, Any]] = operations.copy()
     sorted_operations.sort(key=lambda x: x.get('date', ''), reverse=reverse)
 
     return sorted_operations
-
