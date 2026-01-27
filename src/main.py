0fa10805490fc894ebf780731c34b8aed64ea106
Sergey Belyakov <belserg2002@gmail.com>
2026-01-27 16:30:34 +0300

    изменение src/main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-from src.masks import get_mask_account, get_mask_card_number
 
-card = input("Введите номер карты: ")
-account = input("Введите номер счёта: ")
-
-masked_card = get_mask_card_number(card)
-print(f"Карта: {masked_card}")
-masked_account = get_mask_account(account)
-print(f"Маска: {masked_account}")
